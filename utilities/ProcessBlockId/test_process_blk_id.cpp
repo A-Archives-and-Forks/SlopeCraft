5d843e4eec7d33fcf9cba888efc5e94e2eebb88f
ToKiNoBug <tokinobug@163.com>
2026-04-06 21:30:04 +0800

    Parse and match block id
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- utilities/ProcessBlockId/test_process_blk_id.cpp
+++ utilities/ProcessBlockId/test_process_blk_id.cpp
@@ -6,6 +6,8 @@ using std::cout, std::endl;
 
 bool run_single_test(std::string_view str) noexcept;
 
+bool run_match_test();
+
 int main(int, char**) {
   const std::string_view str[] = {
       "",
@@ -20,9 +22,13 @@ int main(int, char**) {
       "true,down=true]"};
 
   for (auto& id : str) {
-    if (!run_single_test(id)) {
+    if (not run_single_test(id)) {
       return 1;
     }
+  }
+
+  if (not run_match_test()) {
+    return 2;
   }
 
   return 0;
@@ -44,5 +50,52 @@ bool run_single_test(std::string_view str) noexcept {
   }
   cout << endl;
 
+  return true;
+}
+
+bool run_match_test() {
+  struct match_task {
+    std::string parent_id;
+    std::string detail_id;
+    bool should_pass;
+  };
+
+  std::vector<match_task> tasks{
+    {"minecraft:stone", "minecraft:stone", true},
+    {"stone", "minecraft:stone", true},
+    {"stone", "minecraft:not_stone", false},
+    {"stone", "minecraft:stone[]", true},
+    {"minecraft:stone", "minecraft:stone[key=value]", true},
+    {"minecraft:stone", "stone", false},
+    {"minecraft:concrete[color=white]", "minecraft:concrete[color=white]",
+     true},
+    {"minecraft:concrete[color=white]", "minecraft:concrete[color=black]",
+     false},
+    {"minecraft:mushroom_stem",
+     "minecraft:mushroom_stem[east=true,west=true,"
+     "north=true,south=true,up=true,down=true]",
+     true},
+    {"mushroom_stem",
+     "minecraft:mushroom_stem[east=true,west=true,"
+     "north=true,south=true,up=true,down=true]",
+     true},
+    {"minecraft:mushroom_stem[east=true,west=true]",
+     "minecraft:mushroom_stem[east=true,west=true,"
+     "north=true,south=true,up=true,down=true]",
+     true},
+    {"minecraft:mushroom_stem[west=true,east=false]",
+     "minecraft:mushroom_stem[east=true,west=true,"
+     "north=true,south=true,up=true,down=true]",
+     false},
+  };
+
+  for (const auto& task : tasks) {
+    const auto detail = blkid::parse_block_id(task.detail_id).value();
+    const auto parent = blkid::parse_block_id(task.parent_id).value();
+    const bool match = detail.is_derived_from(parent);
+    if (match not_eq task.should_pass) {
+      return false;
+    }
+  }
   return true;
 }
