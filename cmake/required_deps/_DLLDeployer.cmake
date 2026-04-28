c1e1fd890861988b00305bdae9fd61132c1e1dfa
ToKiNoBug <tokinobug@163.com>
2026-04-28 21:12:31 +0800

    Use Shared lib deployer v1.6.0
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- cmake/required_deps/_DLLDeployer.cmake
+++ cmake/required_deps/_DLLDeployer.cmake
@@ -23,21 +23,17 @@ set(DLLD_download_dir ${CMAKE_BINARY_DIR}/3rdParty/DLLDeployer)
 set(DLLD_file ${DLLD_download_dir}/DLLDeployer.cmake)
 set(QD_file ${DLLD_download_dir}/QtDeployer.cmake)
 if (${WIN32})
-    file(DOWNLOAD https://github.com/SlopeCraft/SharedLibDeployer/releases/download/v1.5.1/DLLDeployer.cmake
+    file(DOWNLOAD https://github.com/SlopeCraft/SharedLibDeployer/releases/download/v1.6.0/DLLDeployer.cmake
         ${DLLD_file}
-        EXPECTED_HASH SHA512=255C1390363E74EE0538F9CC003EAA00B039B55CBB2E17E940EE5B365D4F8FADEBAFD0DF21339FC52A10AD61A1F82FB56E080FDC96B5A617F2EAEC6D8770645A
+        EXPECTED_HASH SHA256=a3d05d2174488f9373af02990254c47a75c24ee628c6763ea76b1f97fe02bee3
     )
-    #    SC_download(https://github.com/ToKiNoBug/SharedLibDeployer/releases/download/v1.4.0/DLLDeployer.cmake
-    #        ${DLLD_file})
     include(${DLLD_file})
 endif ()
 
 if (WIN32 OR APPLE)
-    file(DOWNLOAD https://github.com/SlopeCraft/SharedLibDeployer/releases/download/v1.5.1/QtDeployer.cmake
+    file(DOWNLOAD https://github.com/SlopeCraft/SharedLibDeployer/releases/download/v1.6.0/QtDeployer.cmake
         ${QD_file}
-        EXPECTED_HASH SHA512=7A6E7FEE90DEBC0A4E6BDC4CB65D87DF453E8B24C6ED9E188C174BF64C526CA232FA63211CA661E4B26D06A141291AB0AB3CC499B64775B23FC0E622E0CEA0B4
+        EXPECTED_HASH SHA256=ad420f8df9cec5c8df81d9e394f15de81f5095d1472ebae6f487e9000bcc956a
     )
-    #    SC_download(https://github.com/SlopeCraft/DLLDeployer/releases/download/v1.3/QtDeployer.cmake
-    #        ${QD_file})
     include(${QD_file})
 endif ()
