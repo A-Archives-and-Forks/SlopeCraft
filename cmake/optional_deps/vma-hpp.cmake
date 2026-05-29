a34756578b2ade010e0eb8aeb8013c95ddb24d03
ToKiNoBug <tokinobug@163.com>
2026-05-29 19:42:01 +0800

    Update vma dep version
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- cmake/optional_deps/vma-hpp.cmake
+++ cmake/optional_deps/vma-hpp.cmake
@@ -4,8 +4,8 @@ find_package(VulkanMemoryAllocator REQUIRED)
 include(FetchContent)
 FetchContent_Declare(
     VulkanMemoryAllocator-Hpp
-    URL https://github.com/YaaZ/VulkanMemoryAllocator-Hpp/releases/download/v3.3.0%2B2/VulkanMemoryAllocator-Hpp-3.3.0.tar.gz
-    URL_HASH SHA256=44767b5990dd36bdc3934b48a7bda2906a5573199a12b61ef2f0de8e72f988f1 # Copy from the asset list on the release page
+    URL https://github.com/YaaZ/VulkanMemoryAllocator-Hpp/releases/download/v3.3.0%2B3/VulkanMemoryAllocator-Hpp-3.3.0.tar.gz
+    URL_HASH SHA256=8c2a0573babe1f86f3241d12b4c9df40bb944649b7f4239c1c611797f8af6cd5 # Copy from the asset list on the release page
     OVERRIDE_FIND_PACKAGE
 )
 message(STATUS "Downloading YaaZ/VulkanMemoryAllocator-Hpp......")
