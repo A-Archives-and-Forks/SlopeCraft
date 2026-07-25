790b5ff9b80f1bee9e13de7a01aa85a8a2cede59
ToKiNoBug <tokinobug@163.com>
2026-07-25 15:30:19 +0800

    Fetch cli11 and nlohmann json into cmake binary dir / 3rdParty instead of cmake source dir
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- cmake/required_deps/nlohmann_json.cmake
+++ cmake/required_deps/nlohmann_json.cmake
@@ -1,8 +1,8 @@
 set(SlopeCraft_download_njson OFF)
 
 if (NOT DEFINED SlopeCraft_Nlohmann_json_include_dir)
-    if (EXISTS ${CMAKE_SOURCE_DIR}/3rdParty/nlohmann/json.hpp)
-        set(SlopeCraft_Nlohmann_json_include_dir ${CMAKE_SOURCE_DIR}/3rdParty/nlohmann)
+    if (EXISTS ${CMAKE_BINARY_DIR}/3rdParty/nlohmann/json.hpp)
+        set(SlopeCraft_Nlohmann_json_include_dir ${CMAKE_BINARY_DIR}/3rdParty/nlohmann)
     else ()
         set(SlopeCraft_download_njson ON)
     endif ()
@@ -22,11 +22,11 @@ if (${SlopeCraft_download_njson})
     message(STATUS "Downloading nlohmann json...")
     file(DOWNLOAD
         "https://github.com/nlohmann/json/releases/download/v3.11.3/json.hpp"
-        ${CMAKE_SOURCE_DIR}/3rdParty/nlohmann/json.hpp
+        ${CMAKE_BINARY_DIR}/3rdParty/nlohmann/json.hpp
         EXPECTED_HASH SHA512=DA77FA48CA883DACF5CE147B2354E9D957AD66EDF72A7103FF5A8611C5CDA77B64F1F0CA60491295574EE158CECCCFE7797CD36FAAC5B47E75687400AC60769D
         SHOW_PROGRESS)
 
     message(STATUS "nlohmann json downloaded")
     set(SlopeCraft_download_njson OFF)
-    set(SlopeCraft_Nlohmann_json_include_dir ${CMAKE_SOURCE_DIR}/3rdParty/nlohmann)
+    set(SlopeCraft_Nlohmann_json_include_dir ${CMAKE_BINARY_DIR}/3rdParty/nlohmann)
 endif ()
