3cb1f551c50eebb8c44c4aa74752ddc62f92d085
ToKiNoBug <tokinobug@163.com>
2026-07-27 21:21:24 +0800

    Remove version requirement in deb.cmake
    Signed-off-by: ToKiNoBug <tokinobug@163.com>

--- cpack/deb.cmake
+++ cpack/deb.cmake
@@ -10,24 +10,26 @@ if (${SlopeCraft_GPU_API} STREQUAL "OpenCL")
 endif ()
 if (${SlopeCraft_GPU_API} STREQUAL "Vulkan")
     set(SlopeCraft_debian_vulkan_deps
-        libvulkan1)
+        libvulkan1
+    )
 endif ()
 
 set(CPACK_DEBIAN_PACKAGE_DEPENDS
+
     "zlib1g"
     "libzip-dev" # package name like lib*-dev are much more universal in different ubuntu releases
     "libboost-iostreams-dev"
     "libpng-dev"
-    "libqt6core6(>=6.2.4)"
-    "libqt6gui6(>=6.2.4)"
-    "libqt6widgets6(>=6.2.4)"
-    "libqt6network6(>=6.2.4)"
+    "libqt6core6"
+    "libqt6gui6"
+    "libqt6widgets6"
+    "libqt6network6"
     "libopengl0"
-    "libqt6opengl6(>=6.2.4)"
-    "libqt6openglwidgets6(>=6.2.4)"
-    "libqt6dbus6(>=6.2.4)"
+    "libqt6opengl6"
+    "libqt6openglwidgets6"
+    "libqt6dbus6"
     "libglx0"
-    "qt6-qpa-plugins(>=6.2.4)"
+    "qt6-qpa-plugins"
     ${SlopeCraft_debian_opencl_deps}
     ${SlopeCraft_debian_vulkan_deps}
 )
